446bf16fc19096dc4b52adadd8616cb7b56e6964
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-02-24 00:31:06 -0300

    Add index article components

---
+++ src/components/Articulos/ComponentesArticulo/index.js
@@ -0,0 +1,5 @@
+export { Encabezado } from "./Encabezado";
+export { Bajada } from "./Bajada";
+export { Imagen } from "./Imagen";
+export { ImagenAutor } from "./ImagenAutor";
+export { Marquesina } from "./Marquesina";
