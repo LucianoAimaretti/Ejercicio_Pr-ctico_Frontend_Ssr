7c16f96d3fd3ef06692a0e3c3d8bf0c469cdd674
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-02-24 00:35:01 -0300

    🤯 - Add article sorter

---
+++ src/components/Articulos/TipoArticulo.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./stylesComponentes.scss";
+import { Encabezado, Bajada, Imagen, ImagenAutor, Marquesina } from "./ComponentesArticulo/index";
+
+const AdministradorComponentes = (datos) => ({
+    Encabezado: () => <Encabezado url={datos.url} volanta={datos.volanta} titulo={datos.titulo} />,
+    Bajada: () => <Bajada url={datos.url} bajada={datos.bajada} />,
+    Imagen: () => <Imagen url={datos.url} imagen={datos.imagen} alt={datos.titulo} />,
+    ImagenAutor: () => <ImagenAutor url={datos.url} imagenAutor={datos.imagenAutor} nombreAutor={datos.nombreAutor} />,
+    Marquesina: () => <Marquesina url={datos.url} nombreAutor={datos.nombreAutor} marquesina={datos.marquesina} />,
+});
+
+function TipoArticulo({ tipo, datos }) {
+    const { Encabezado, Bajada, Imagen, ImagenAutor, Marquesina } = AdministradorComponentes(datos);
+    switch (tipo) {
+        case "autor":
+            return (
+                <>
+                    <Encabezado />
+                    <ImagenAutor />
+                    <Marquesina />
+                </>
+            );
+        case "invertida":
+            return (
+                <>
+                    <Encabezado />
+                    <Bajada />
+                    <Marquesina />
+                    <Imagen />
+                </>
+            );
+        default:
+            return (
+                <>
+                    <Imagen />
+                    <Encabezado />
+                    <Bajada />
+                    <Marquesina />
+                </>
+            );
+    }
+}
+
+export default TipoArticulo;
