9439c6d171388a6d9545b6374c9e0e218b8ce58f
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-02-24 16:30:44 -0300

    Update file

--- src/components/Articulos/Articulo.js
+++ src/components/Articulos/Articulo.js
@@ -2,10 +2,10 @@ import React from "react";
 import "../../sass/app.scss";
 import ClasificadorArticulos from "./ClasificadorArticulos";
 
-function Articulo({ articulos }) {
+function Articulo({ articulos, tipo }) {
     return (
-        <article className="article_container">
-            <ClasificadorArticulos tipo={articulos.tipo} datos={articulos} />
+        <article className={`article_container ${tipo ? tipo : null}`}>
+            <ClasificadorArticulos tipo={tipo} datos={articulos} />
         </article>
     );
 }
