ddef0b78982ba8e7f0fe2f474cac5ab96a15118d
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-02-24 14:53:56 -0300

    Update files

--- src/components/Modulos/ClasificadorModulos.js
+++ src/components/Modulos/ClasificadorModulos.js
@@ -6,30 +6,30 @@ function ClasificadorModulos({ tipo, articulos }) {
         case "notas_2":
             return (
                 <>
-                    <Articulo articulos={articulos[0]} />
-                    <Articulo articulos={articulos[1]} />
+                    <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
+                    <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
                 </>
             );
         case "notas_3":
             return (
                 <>
-                    <Articulo articulos={articulos[0]} />
-                    <Articulo articulos={articulos[1]} />
-                    <Articulo articulos={articulos[2]} />
+                    <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
+                    <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
+                    <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
                 </>
             );
         case "notas_6":
             return (
                 <>
                     <div className="fila">
-                        <Articulo articulos={articulos[0]} />
-                        <Articulo articulos={articulos[1]} />
-                        <Articulo articulos={articulos[2]} />
+                        <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
+                        <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
+                        <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
                     </div>
                     <div className="fila">
-                        <Articulo articulos={articulos[3]} />
-                        <Articulo articulos={articulos[4]} />
-                        <Articulo articulos={articulos[5]} />
+                        <Articulo articulos={articulos[3]} tipo={articulos[3].tipo} />
+                        <Articulo articulos={articulos[4]} tipo={articulos[4].tipo} />
+                        <Articulo articulos={articulos[5]} tipo={articulos[5].tipo} />
                     </div>
                 </>
             );
@@ -37,19 +37,19 @@ function ClasificadorModulos({ tipo, articulos }) {
             return (
                 <>
                     <div className="fila">
-                        <Articulo articulos={articulos[0]} />
-                        <Articulo articulos={articulos[1]} />
-                        <Articulo articulos={articulos[2]} />
+                        <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
+                        <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
+                        <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
                     </div>
                     <div className="fila">
-                        <Articulo articulos={articulos[3]} />
-                        <Articulo articulos={articulos[4]} />
-                        <Articulo articulos={articulos[5]} />
+                        <Articulo articulos={articulos[3]} tipo={articulos[3].tipo} />
+                        <Articulo articulos={articulos[4]} tipo={articulos[4].tipo} />
+                        <Articulo articulos={articulos[5]} tipo={articulos[5].tipo} />
                     </div>
                     <div className="fila">
-                        <Articulo articulos={articulos[6]} />
-                        <Articulo articulos={articulos[7]} />
-                        <Articulo articulos={articulos[8]} />
+                        <Articulo articulos={articulos[6]} tipo={articulos[6].tipo} />
+                        <Articulo articulos={articulos[7]} tipo={articulos[7].tipo} />
+                        <Articulo articulos={articulos[8]} tipo={articulos[8].tipo} />
                     </div>
                 </>
             );
