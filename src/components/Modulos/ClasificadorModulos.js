dcd1172c3fefd4f10cf07c32b8fc4938f03e6bfe
LucianoAimaretti <lucianoaimaretti@gmail.com>
2022-02-25 01:39:35 -0300

    Update files

--- src/components/Modulos/ClasificadorModulos.js
+++ src/components/Modulos/ClasificadorModulos.js
@@ -18,38 +18,15 @@ function ClasificadorModulos({ tipo, articulos }) {
                     <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
                 </>
             );
-        case "notas_6":
+
+        case "focal-derecho":
             return (
                 <>
-                    <div className="fila">
-                        <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
-                        <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
-                        <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
+                    <div className="parte_izquierda">
+                        <Articulo articulos={articulos[0]} />
                     </div>
-                    <div className="fila">
-                        <Articulo articulos={articulos[3]} tipo={articulos[3].tipo} />
-                        <Articulo articulos={articulos[4]} tipo={articulos[4].tipo} />
-                        <Articulo articulos={articulos[5]} tipo={articulos[5].tipo} />
-                    </div>
-                </>
-            );
-        case "notas_9":
-            return (
-                <>
-                    <div className="fila">
-                        <Articulo articulos={articulos[0]} tipo={articulos[0].tipo} />
-                        <Articulo articulos={articulos[1]} tipo={articulos[1].tipo} />
-                        <Articulo articulos={articulos[2]} tipo={articulos[2].tipo} />
-                    </div>
-                    <div className="fila">
-                        <Articulo articulos={articulos[3]} tipo={articulos[3].tipo} />
-                        <Articulo articulos={articulos[4]} tipo={articulos[4].tipo} />
-                        <Articulo articulos={articulos[5]} tipo={articulos[5].tipo} />
-                    </div>
-                    <div className="fila">
-                        <Articulo articulos={articulos[6]} tipo={articulos[6].tipo} />
-                        <Articulo articulos={articulos[7]} tipo={articulos[7].tipo} />
-                        <Articulo articulos={articulos[8]} tipo={articulos[8].tipo} />
+                    <div className="parte_derecha">
+                        <Articulo articulos={articulos[1]} />
                     </div>
                 </>
             );
